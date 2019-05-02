e5ea35ff1ebdc5a667e2b8a588f9289795b9db81
Ikuo Terado <eqobar@gmail.com>
2019-05-02 11:35:05 +0900

    Minor changes

--- examples/simple/src/index.js
+++ examples/simple/src/index.js
@@ -1,13 +1,5 @@
 import { CoreCube } from 'toio-webble'
 
-document.getElementById('app').innerHTML = `
-<div>
-  <button id="discover">Discover</button>
-  <button id="forward">Forward</button>
-  <button id="backward">Backward</button>
-</div>
-`
-
 document.querySelector('#discover').addEventListener('click', () => {
   CoreCube.discover().then(cube => {
     document.querySelector('#forward').addEventListener('click', () => {
